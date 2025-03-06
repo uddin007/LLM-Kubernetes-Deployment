df35c5e2d8c1dad03a4f22f11a959e3df1c24a75
Salah Uddin <salah.uddin@mail.mcgill.ca>
2025-03-06 12:37:58 -0500

    Commit cluster config

---
+++ cluster-config.sh
@@ -0,0 +1,8 @@
+gcloud config set project ${{ secrets.GCP_PROJECT_ID }}
+export PROJECT_ID=$(gcloud config get project)
+export REGION=us-east4
+gcloud container clusters create llm-deploy --location ${REGION} \
+--workload-pool ${PROJECT_ID}.svc.id.goog   --enable-image-streaming   --node-locations=$REGION-a \
+--workload-pool=${PROJECT_ID}.svc.id.goog   --machine-type n2d-standard-4   --num-nodes 1  \
+--release-channel=rapid
+gcloud container clusters get-credentials llm-deploy --region=${REGION}

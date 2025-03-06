c756a1b925e0ebb9350fffe47d2ecfaa40c9e44d
Salah Uddin <salah.uddin@mail.mcgill.ca>
2025-03-06 12:49:37 -0500

    updated bash script

--- cluster-config.sh
+++ cluster-config.sh
@@ -1,8 +1,17 @@
-gcloud config set project ${secrets.GCP_PROJECT_ID}
+#!/bin/bash
+
+# Set the GCP project
+gcloud config set project ${GCP_PROJECT_ID}
+
+# Export the project ID and region
 export PROJECT_ID=$(gcloud config get project)
 export REGION=us-east4
+
+# Create the GKE cluster
 gcloud container clusters create llm-deploy --location ${REGION} \
---workload-pool ${PROJECT_ID}.svc.id.goog   --enable-image-streaming   --node-locations=$REGION-a \
---workload-pool=${PROJECT_ID}.svc.id.goog   --machine-type n2d-standard-4   --num-nodes 1  \
+--workload-pool ${PROJECT_ID}.svc.id.goog --enable-image-streaming --node-locations=$REGION-a \
+--machine-type n2d-standard-4 --num-nodes 1 \
 --release-channel=rapid
+
+# Get cluster credentials
 gcloud container clusters get-credentials llm-deploy --region=${REGION}
